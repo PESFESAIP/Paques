ecb0d02299bffde8c7cfd6ab20b277c05e327dd3
Destructis <107251998+Destructis@users.noreply.github.com>
2024-02-14 16:55:31 +0100

    texte

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
   // Ajouter un écouteur d'événement pour le clic sur le mot cible
   motCible1.addEventListener("click", function () {
     // Afficher une alerte
-    alert("Vous avez cliqué sur le mot cliquable 1 !");
+    alert(
+      "Créée en 2004, l’association AIDER a pour objectif de trouver des solutions pour répondre aux besoins des familles défavorisées de Madagascar. Grâce aux dons des adhérents, AIDER propose et lance des actions qui répondent aux besoins alimentaires et technologiques des familles malgaches. Sur place, des membres de l’association, conseillent et forment les populations touchées par la pauvreté, dans leur vie quotidienne. Les dons permettent aux animateurs de mettre en place des projets tels que  jardins familiaux, parrainage scolaire, enseignement technologique et professionnel, ferme expérimentale."
+    );
   });
 });
 
@@ -16,6 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Ajouter un écouteur d'événement pour le clic sur le mot cible
   motCible2.addEventListener("click", function () {
     // Afficher une alerte
-    alert("Vous avez cliqué sur le mot cliquable 2 !");
+    alert(
+      "LIBAMI est une association à but non lucratif (loi 1901). Elle vient en aide aux familles les plus pauvres du quartier de Nabaa, banlieue des plus défavorisées de Beyrouth. Créée en 1986, cette structure, aconfessionnelle et apolitique, est reconnue d’intérêt général en France et par le Ministère des Affaires Sociales au Liban."
+    );
   });
 });
